5c52648e1eb2e5f24e7b32af8f14b9c84684b24c
Paulson McIntyre <paul@gpmidi.net>
2018-02-07 01:25:43 -0500

    Updated pipenv's lock and ignored more garbage. Also started filling in models.

--- pfsense/models.py
+++ pfsense/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+### Ports ###
+class TUPort(models.Model):
+    PROTO_TCP = 'tcp'
+    PROTO_UDP = 'udp'
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(unique=False, null=False, blank=False, help_text="Port common name")
+    proto = models.CharField(null=False, default=PROTO_TCP, help_text="IP Protocol", choices=(PROTO_TCP, PROTO_UDP))
+    port = models.IntegerField(null=False, default=None, help_text="TCP or UDP port number")
+
+
+class PortGroup(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(unique=True, null=False, blank=False, help_text="Port group name")
+    description = models.TextField(null=False, blank=True, help_text="Extended details")
+    # All ports in this group
+    ports = models.ManyToManyField(TUPort)
+
+
+### Firewalls ###
+
+class FirewallHardwareClass(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(unique=True, null=False, blank=False, help_text="Hardware Class Name")
+    description = models.TextField(null=False, blank=True, help_text="Extended details")
+
+
+class FirewallHardware(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(unique=True, null=False, blank=False, help_text="Hardware Name")
+    description = models.TextField(null=False, blank=True, help_text="Extended details")
+    hwclass = models.ForeignKey(FirewallHardwareClass, null=False, help_text="Base hardware", on_delete=models.CASCADE)
+
+    one_gbe_interface_count = models.IntegerField(null=True, default=None, help_text="Number of 1GbE interfaces")
+    ten_gbe_interface_count = models.IntegerField(null=True, default=None, help_text="Number of 10GbE interfaces")
+
+    ram_gb = models.IntegerField(null=True, default=None, help_text="Amount of installed RAM in GiB")
+
+
+class Firewall(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    hw = models.ForeignKey(FirewallHardware, null=False, help_text="Base hardware", on_delete=models.CASCADE)
+    name = models.CharField(unique=True, null=False, blank=False, help_text="Asset Name")
+    hostname = models.CharField(unique=True, null=False, blank=False, help_text="Asset hostname")
+    description = models.TextField(null=False, blank=True, help_text="Extended details")
+    asset_tag = models.CharField(unique=True, null=False, blank=False, help_text="Asset tag")
+    org_asset_tag = models.CharField(unique=True, null=False, blank=False,
+                                     help_text="Asset id from salesforce org's asset object")
+
+
+class LogicalFWInterface(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    ifname = models.CharField(unique=False, null=False, blank=False, help_text="Interface name")
+    ip = models.GenericIPAddressField(unique=False, null=False, blank=False, help_text="IPv4 Address")
+    netmask = models.GenericIPAddressField(unique=False, null=False, blank=False, default=None,
+                                           help_text="Interface's netmask")
+    ip = models.GenericIPAddressField(unique=False, null=False, blank=False, help_text="IPv4 Address")
+    firewall = models.ForeignKey(Firewall, null=False, help_text="Firewall this interface belongs to",
+                                 on_delete=models.CASCADE)
