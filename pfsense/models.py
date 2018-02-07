d1496b1931dc9a15213ff609b8a15c51f55d43f8
Paulson McIntyre <paul@gpmidi.net>
2018-02-07 01:37:31 -0500

    Fixing chocies and max_lens for charfields

--- pfsense/models.py
+++ pfsense/models.py
@@ -8,14 +8,15 @@ class TUPort(models.Model):
     PROTO_UDP = 'udp'
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(unique=False, null=False, blank=False, help_text="Port common name")
-    proto = models.CharField(null=False, default=PROTO_TCP, help_text="IP Protocol", choices=(PROTO_TCP, PROTO_UDP))
+    name = models.CharField(max_length=255, unique=False, null=False, blank=False, help_text="Port common name")
+    proto = models.CharField(max_length=3, null=False, default=PROTO_TCP, help_text="IP Protocol",
+                             choices=((PROTO_TCP, 'TCP'), (PROTO_UDP, 'UDP')))
     port = models.IntegerField(null=False, default=None, help_text="TCP or UDP port number")
 
 
 class PortGroup(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(unique=True, null=False, blank=False, help_text="Port group name")
+    name = models.CharField(max_length=255, unique=True, null=False, blank=False, help_text="Port group name")
     description = models.TextField(null=False, blank=True, help_text="Extended details")
     # All ports in this group
     ports = models.ManyToManyField(TUPort)
@@ -25,13 +26,13 @@ class PortGroup(models.Model):
 
 class FirewallHardwareClass(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(unique=True, null=False, blank=False, help_text="Hardware Class Name")
+    name = models.CharField(max_length=255, unique=True, null=False, blank=False, help_text="Hardware Class Name")
     description = models.TextField(null=False, blank=True, help_text="Extended details")
 
 
 class FirewallHardware(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(unique=True, null=False, blank=False, help_text="Hardware Name")
+    name = models.CharField(max_length=255, unique=True, null=False, blank=False, help_text="Hardware Name")
     description = models.TextField(null=False, blank=True, help_text="Extended details")
     hwclass = models.ForeignKey(FirewallHardwareClass, null=False, help_text="Base hardware", on_delete=models.CASCADE)
 
@@ -44,17 +45,17 @@ class FirewallHardware(models.Model):
 class Firewall(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     hw = models.ForeignKey(FirewallHardware, null=False, help_text="Base hardware", on_delete=models.CASCADE)
-    name = models.CharField(unique=True, null=False, blank=False, help_text="Asset Name")
-    hostname = models.CharField(unique=True, null=False, blank=False, help_text="Asset hostname")
+    name = models.CharField(max_length=255, unique=True, null=False, blank=False, help_text="Asset Name")
+    hostname = models.CharField(max_length=4096, unique=True, null=False, blank=False, help_text="Asset hostname")
     description = models.TextField(null=False, blank=True, help_text="Extended details")
-    asset_tag = models.CharField(unique=True, null=False, blank=False, help_text="Asset tag")
-    org_asset_tag = models.CharField(unique=True, null=False, blank=False,
+    asset_tag = models.CharField(max_length=4096, unique=True, null=False, blank=False, help_text="Asset tag")
+    org_asset_tag = models.CharField(max_length=18, unique=True, null=False, blank=False,
                                      help_text="Asset id from salesforce org's asset object")
 
 
 class LogicalFWInterface(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    ifname = models.CharField(unique=False, null=False, blank=False, help_text="Interface name")
+    ifname = models.CharField(max_length=255, unique=False, null=False, blank=False, help_text="Interface name")
     ip = models.GenericIPAddressField(unique=False, null=False, blank=False, help_text="IPv4 Address")
     netmask = models.GenericIPAddressField(unique=False, null=False, blank=False, default=None,
                                            help_text="Interface's netmask")
