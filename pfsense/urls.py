ead0289f0f4ff7fc398e862672066596df3e7d10
Paulson McIntyre <paul@gpmidi.net>
2018-02-07 01:33:00 -0500

    Putting pf files under /pf

--- pfsense/urls.py
+++ pfsense/urls.py
@@ -16,9 +16,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-import pfsense
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('pf/', pfsense.urls),
+
 ]
